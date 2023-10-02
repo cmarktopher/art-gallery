d3d2b4b7b9a19eb8a00d29b4cbc8516aa614fc52
chris-marktopher <cmarktopher@gmail.com>
2023-10-02 15:54:39 +1100

    Added axios

---
+++ art-gallery-api/src/services/AuthenticationService.js
@@ -0,0 +1,11 @@
+class AuthenticationService {
+
+    constructor(userDataAccessObject) {
+
+        this.userDataAccess = userDataAccessObject;
+    }
+
+}
+
+
+module.exports = AuthenticationService;
